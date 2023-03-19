34b9cd0030dbfe2d0bec3600067c3b2a082223fa
Asa Holland <asaleholland@gmail.com>
2023-03-18 19:56:01 -0700

    feat(Final Wrap): Failed movei file is created

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 # import os
 
 def get_movie_script_url(movie_title):
@@ -22,19 +22,40 @@ def get_movie_script_url(movie_title):
     return f"{base_url}{url_title}{url_suffix}"
 
 
-def get_movie_script_text(script_url):
-    # Returns the movie script text in dialogue form from the script URL
-    pass
+def fetch_movie_script_text(url, run=False):
+    if not run:
+        return ""
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return pre_element.text if (pre_element := soup.find('pre')) else ""
 
-def save_movie_script_text(movie_title, script_text):
+def save_movie_script_text(movie_title, script_text) -> bool:
+    if not script_text:
+        return False
     # Saves the movie script text to a txt file with the movie title as filename
-    pass
+    with open(f"{movie_title}.txt", 'w') as f:
+        f.write(script_text)
+    return True
 
-if __name__ == '__main__':
-    # List of 641 movie titles
+def main(is_requesting=False):
     movie_titles = ["American Beauty", "The Godfather II", "The Lord of the Rings: The Fellowship of the Ring"]
 
+    print(f'Processing {len(movie_titles)} movies...')
+
+    failures = []
     for movie_title in movie_titles:
         script_url = get_movie_script_url(movie_title)
-        script_text = get_movie_script_text(script_url)
-        save_movie_script_text(movie_title, script_text)
+        script_text = fetch_movie_script_text(script_url)
+        if not save_movie_script_text(movie_title, script_text):
+            failures.append(movie_title)
+    if failures:
+        print(f'Failed to process {len(failures)} movies')
+        # write a new text file with the failed movies
+        with open('failed_movies.txt', 'w') as f:
+            f.write('\n'.join(failures))
+    else:
+        print('All movies processed successfully')
+
+
+if __name__ == '__main__':
+    main(is_requesting=True)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from main import get_movie_script_url
-
+from main import get_movie_script_url,save_movie_script_text, main
+import os
 
 def test_get_movie_script_url():
     movie_title = "American Beauty"
@@ -26,3 +26,29 @@ def test_should_handle_longer_title():
     expected_url = "https://imsdb.com/scripts/Lord-of-the-Rings-Fellowship-of-the-Ring,-The.html"
 
     assert get_movie_script_url(movie_title) == expected_url
+
+
+
+def test_sample_text_should_write_to_file():
+    movie_title = "American Beauty"
+    script_text = "This is a sample script text"
+    expected_file_name = "American Beauty.txt"
+
+    save_movie_script_text(movie_title, script_text)
+
+    assert os.path.exists(expected_file_name)
+
+    with open(expected_file_name, 'r') as f:
+        assert f.read() == script_text
+
+    os.remove(expected_file_name)
+
+def test_should_main_run():
+    main(is_requesting=False)
+    # assert that a failed_movies.txt file was created
+    # assert that it contains the movie title "American Beauty"
+    assert os.path.exists('failed_movies.txt')
+    with open('failed_movies.txt', 'r') as f:
+        assert 'American Beauty' in f.read()
+
+    os.remove('failed_movies.txt')
